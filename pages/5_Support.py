c82df0ba78238201f50178d3a1735fa9a996274b
AgentAiDrive <agentaidrive@gmail.com>
2025-07-29 03:32:19 -0700

    Update 5_Support.py

--- pages/5_Support.py
+++ pages/5_Support.py
@@ -5,7 +5,7 @@ apply_phone_style()
 
 st.title("🆘 Support & Help")
 st.markdown("""
-- [📖 User Manual](https://www.myparenthelpers.com/manual)
-- [📬 Contact Us](mailto:support@myparenthelpers.com)
-- [🔐 Privacy Policy](https://www.myparenthelpers.com/privacy)
+- [📖 User Manual](https://www.myparenthelpers.com/support)
+- [📬 Contact Us](mailto: myparenthelpers@gmail.com)
+- [🔐 Privacy Policy](https://myparenthelpers.com/support#215d9772-60eb-496e-a01b-97ae7ace9c67)
 """)
